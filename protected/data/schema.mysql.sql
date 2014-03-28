8d25226c02a329f4e517c51073d0dfd59006d530
voldemar <voldemar.duketskiy@gmail.com>
2014-03-28 03:47:48 +0300

    обновил схему

--- protected/data/schema.mysql.sql
+++ protected/data/schema.mysql.sql
@@ -1,3 +1,44 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Версия 4096
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Адрес: localhost (MySQL 5.5.27)
+# Схема: apfin-test
+# Время создания: 2014-03-28 00:46:47 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Дамп таблицы bills
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `bills`;
+
+CREATE TABLE `bills` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `currency_id` int(10) NOT NULL,
+  `amount` float NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `bills_unique_user_currency_pair` (`user_id`,`currency_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Дамп таблицы country
+# ------------------------------------------------------------
+
 DROP TABLE IF EXISTS `country`;
 
 CREATE TABLE `country` (
@@ -18,6 +59,24 @@ CREATE TABLE `currency_rates` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `currency` varchar(20) NOT NULL,
   `rate` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Дамп таблицы currency_trans
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `currency_trans`;
+
+CREATE TABLE `currency_trans` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `from_currency_id` int(10) NOT NULL,
+  `to_currency_id` int(10) NOT NULL,
+  `amount` float NOT NULL,
+  `date` datetime NOT NULL,
+  `converted_amount` float NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -66,3 +125,13 @@ CREATE TABLE `user` (
   `country_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
